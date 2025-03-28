f6ed79b99bb5b5c39bba3c9b012ce89f402c212c
joaonogureira <joaonogueiracontacto@gmail.com>
2025-03-28 18:27:54 +0000

    JN | created transaction model and created the main controller

---
+++ src/main/java/com/bank/bank/Controllers/BankController.java
@@ -0,0 +1,9 @@
+package com.bank.bank.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class BankController {
+}
