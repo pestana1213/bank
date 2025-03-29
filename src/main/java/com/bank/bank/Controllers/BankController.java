8f02d92b47937f9aeb861762feebb75961ca588a
joaonogureira <joaonogueiracontacto@gmail.com>
2025-03-29 11:03:01 +0000

    JN | Added the logic for calculating the fee and changed the validateUpdateTransaction logic

--- src/main/java/com/bank/bank/Controllers/BankController.java
+++ src/main/java/com/bank/bank/Controllers/BankController.java
@@ -1,9 +1,14 @@
 package com.bank.bank.Controllers;
 
+import com.bank.bank.Service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transactions")
 public class BankController {
+
+    @Autowired
+    TransactionService transactionService;
 }
